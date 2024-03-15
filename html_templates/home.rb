d4943a264aa4021921a116d8590e628e9493a642
4hm3d4buh4ss4n <ahm3d.abu.hassan@gmail.com>
2024-03-15 12:03:03 +0100

    Main.rb wurde um POST für Einloggen erweitert

--- html_templates/home.rb
+++ html_templates/home.rb
@@ -1,7 +1,15 @@
-def head()
+def head(titel) #Style?
     <<~STR
         <head>
-            
+            <title>#{titel}</title>
         </head>
+    STR
+end
+def login()
+    <<~STR
+        <form action="/login" method="POST">
+            <label for="username">Username:</label><input type="text" id="username" name="username">
+            <label for="password">Password:</label><input type="password" id="password" name="password">
+        </form>
     STR
 end
