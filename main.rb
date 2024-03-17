690aa0b65946553c6984a3b056e36ae90574b92e
4hm3d4buh4ss4n <ahm3d.abu.hassan@gmail.com>
2024-03-17 16:14:59 +0100

    Die Cookies wurden implementiert

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 require 'socket'
 require 'uri'
+require 'net/http'
 require './globalFunctions.rb'
 require './html_templates/home.rb'
 
 server = TCPServer.new(8080)
-
+cookies = {
+    :sUser => nil,
+    :sInt => rand(100)
+}
 loop do
     client = server.accept
     request_line = client.readline
@@ -16,7 +20,6 @@ loop do
         status_code = '200 OK'
         response_message = home('HOME.', 'Falsches PASSWORD.')
     when ['POST', '/login']
-        status_code = '200 OK'
         puts target
         headers = {}
         while true 
@@ -30,13 +33,37 @@ loop do
         user = VerifyUser.new(userData["username"], userData["password"])
         if user.verify()
             puts "Ok"
+            status_code = '303 OK'
+            cookies[:sUser] = "sid=#{userData["username"]}_#{cookies[:sInt]}" #max-age=5000 
+            lock = "Location: http://127.0.0.1:8080/inside"
         else 
             puts "Nope."
+            status_code = '303 OK'
+            lock = "Location: http://127.0.0.1:8080/home"
+        end
+    when ['GET','/inside']
+        saveH = {}
+        while true 
+            findCookie = client.readline 
+            break if findCookie == "\r\n"
+            header_name, value = findCookie.split(": ")
+            saveH[header_name] = value
+        end
+        if  cookies[:sUser] == nil || saveH == nil
+            puts "Kein Cookie."
+            status_code = "303 OK"
+            lock = "Location: http://127.0.0.1:8080/home"
+        else 
+            status_code = '200 OK'
+            lock = nil
+            response_message = "Hello, Insider..."
         end
     end
     http_response = <<~MSG
     HTTP/1.1 #{status_code}
     Content-Type: text/html; charset=utf-8
+    #{lock}
+    Set-cookie: #{cookies[:sUser]}
 
     #{response_message}
     MSG
