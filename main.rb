2ef287b02525743f7fe57741254f3e4eba894d0b
4hm3d4buh4ss4n <ahm3d.abu.hassan@gmail.com>
2024-03-20 14:36:00 +0100

    die Logout Funktion wurde eingerichtet

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'socket'
 require 'uri'
 require 'net/http'
 require './globalFunctions.rb'
-require './html_templates/home.rb'
+require './html_templates/template.rb'
 
 server = TCPServer.new(8080)
 cookies = {
@@ -19,8 +19,13 @@ loop do
     when ['GET','/home']
         status_code = '200 OK'
         response_message = home('HOME.', 'Falsches PASSWORD.')
+        response_message << "<script>"
+        f = File.open("./js/test.js")
+            content = f.read
+            response_message << content
+        f.close
+        response_message << "</script>"
     when ['POST', '/login']
-        puts target
         headers = {}
         while true 
             line = client.readline 
@@ -31,17 +36,19 @@ loop do
         body = client.read(headers['Content-Length'].to_i)
         userData = URI.decode_www_form(body).to_h # :username, :password
         user = VerifyUser.new(userData["username"], userData["password"])
+        userData = user.connect()
         if user.verify()
             puts "Ok"
             status_code = '303 OK'
-            cookies[:sUser] = "sid=#{userData["username"]}_#{cookies[:sInt]}" #max-age=5000 
-            lock = "Location: http://127.0.0.1:8080/inside"
+            cookies[:sUser] = "sid=#{userData["Username"]}_#{cookies[:sInt]}" #max-age=60 
+            lock = "Location: http://127.0.0.1:8080/inside/#{userData["Username"]}"
         else 
             puts "Nope."
             status_code = '303 OK'
             lock = "Location: http://127.0.0.1:8080/home"
         end
-    when ['GET','/inside']
+    when ['GET',target[/^\/inside\/[0-9a-zA-Z]+/]] # target[/^\/inside\/[0-9a-zA-Z]+/]
+        nickname = target.slice!(/[a-z0-9]+$/)
         saveH = {}
         while true 
             findCookie = client.readline 
@@ -49,21 +56,29 @@ loop do
             header_name, value = findCookie.split(": ")
             saveH[header_name] = value
         end
-        if  cookies[:sUser] == nil || saveH == nil
+        saveH["Cookie"].strip
+        if cookies[:sUser] != saveH["Cookie"].strip || saveH["Cookie"] == nil
             puts "Kein Cookie."
             status_code = "303 OK"
             lock = "Location: http://127.0.0.1:8080/home"
         else 
+            puts "Es is ok."
             status_code = '200 OK'
             lock = nil
-            response_message = "Hello, Insider..."
+            response_message = headInside('Overview',nickname) 
         end
+    when ['GET', '/out']
+        puts "Logout.."
+        cookies[:sUser] = nil
+        timeC = "max-age=-60" 
+        status_code = '303 OK'
+        lock = "Location: http://127.0.0.1:8080/home"
     end
     http_response = <<~MSG
     HTTP/1.1 #{status_code}
     Content-Type: text/html; charset=utf-8
     #{lock}
-    Set-cookie: #{cookies[:sUser]}
+    Set-cookie: #{cookies[:sUser]};#{timeC}
 
     #{response_message}
     MSG
