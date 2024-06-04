dde2df325279698824220c382775236cd576bb60
ahm3dabuhassan <ahm3d.abu.hassan@gmail.com>
2024-06-04 10:07:32 +0200

    History

--- globalFunctions.rb
+++ globalFunctions.rb
@@ -1,10 +1,11 @@
 require 'mysql'
 require 'BCrypt'
 require 'socket'
+require 'yaml/store'
 
 class VerifyUser 
     DB_Data = {
-        :user => 'root', :password => 'airbag2323_23', :host => 'localhost', :dbname => 'kino', :table_name => 'users'
+        :user => 'root', :password => 'Sh0-3_G4z-3', :host => 'localhost', :dbname => 'kino', :table_name => 'users'
     }
     @@v = {
         :connection => nil, :response => nil, :command => nil, :verify => nil
@@ -116,6 +117,7 @@ class FindAllFolders
   
      attr :pattern, true
      attr :allDirectories 
+     
  end
 
  class Overview 
@@ -130,15 +132,22 @@ class FindAllFolders
         @output = nil
         self.init(@index)
     end
-    def init(index) #path als Parameter, wenn am Ende ist, auslesen. 
-        @data.each { |k,v|    
+    def init(index) 
+        @data.each { |k,v| 
             if k == @user  
                 Dir.chdir("#{v[index]}")
                 @output = <<~STR
+                <div id="inside-message"><p></p></div>
                 <div id="working-directory">
                 <p style="color:orange;"><span class="material-symbols-outlined">
                 folder_data
                 </span>Working Directory:<p id="working-directory-path">#{Dir.getwd}</p></p>
+                </div>
+                <div id="history">
+                <span class="material-symbols-outlined">history</span>
+                <div id="history-trigger">
+                    <p>Show history</p>
+                </div>
                 </div>
                 <div id="overview"> 
                     <table>
@@ -158,25 +167,25 @@ class FindAllFolders
                 if @@stats.file?
                         @output << "<tr>"
                         @output <<  <<~STR 
-                            <td><a class="overview-type-file" href="/open-file/#{v[index][/#{k}\/?[a-zA-Z\/\.]*$/]}/#{f}"><span class="material-symbols-outlined">
+                            <td><a class="overview-type-file" id="file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}" href="/open-file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
                             description
                             </span>#{f}</a></td>
                             <td><p style="width:90%;">#{@@forPath}/<span style="color:#FF9933">#{f}</span></p></td>
                             <td>#{@@stats.size} Bit</td>
                             <td>#{@@stats.atime}</td>
-                            <td><div class="inside-action" id="file/#{@user}/#{v[index][/[a-zA-Z0-9\-\_]+$/]}/#{f}"><span class="material-symbols-outlined">
+                            <td><div class="inside-action" id="file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
                             rebase
                             </span></div></td>
                         STR
                     else 
                         @output << <<~STR
-                            <td><a class="overview-type-directory" href="/open-dir/#{f}"><span class="material-symbols-outlined">
+                            <td><a class="overview-type-directory" id="directory#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}" href="/open-dir#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
                             folder
                             </span>#{f}</a></td>
                             <td><p style="width:90%;">#{@@forPath}/<span style="color:#FF9933">#{f}</span></p></td>
                             <td>#{@@stats.size / 1024} Bit</td>
                             <td>#{@@stats.atime.strftime("%m/%d/%Y at %I:%M %p")}</td>
-                            <td><div class="inside-action" id="directory/#{@user}/#{v[index][/[a-zA-Z0-9\-\_]+$/]}/#{f}"><span class="material-symbols-outlined">
+                            <td><div class="inside-action" id="directory#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
                             rebase
                             </span></div></td>
                         STR
@@ -188,9 +197,93 @@ class FindAllFolders
         } 
         @output << implementJS('inside.js')
     end
+
+    def update(index, directory=nil) 
+        @data.each { |k, v| 
+        if k == @user  
+            Dir.chdir("#{v[index]}")
+            @output = <<~STR
+                <table>
+                    <thead>
+                        <td class="inside-thead">Name:</td>
+                        <td class="inside-thead">Path:</td>
+                        <td class="inside-thead">Size</td>
+                        <td class="inside-thead">Time:</td>
+                        <td class="inside-thead">Task:</td>
+                    </thead>
+        STR
+            @@forPath = Dir.getwd
+            @@folder = Dir.glob("*")
+            @@folder.each { |f| 
+            @@forPath = "#{@@forPath}"
+            @@stats = File.stat("#{f}")
+            if @@stats.file?
+                    @output << "<tr>"
+                    @output <<  <<~STR 
+                        <td><a class="overview-type-file" onclick="taskFile.build();" id="file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}" href="/open-file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
+                        description
+                        </span>#{f}</a></td>
+                        <td><p style="width:90%;">#{@@forPath}/<span style="color:#FF9933">#{f}</span></p></td>
+                        <td>#{@@stats.size} Bit</td>
+                        <td>#{@@stats.atime}</td>
+                        <td><div class="inside-action" id="file#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
+                        rebase
+                        </span></div></td>
+                    STR
+                else 
+                    @output << <<~STR
+                        <td><a class="overview-type-directory" id="directory#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}" href="/open-dir#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
+                        folder
+                        </span>#{f}</a></td>
+                        <td><p style="width:90%;">#{@@forPath}/<span style="color:#FF9933">#{f}</span></p></td>
+                        <td>#{@@stats.size / 1024} Bit</td>
+                        <td>#{@@stats.atime.strftime("%m/%d/%Y at %I:%M %p")}</td>
+                        <td><div class="inside-action" id="directory#{v[index][/(?<=[a-zA-Z\/]\/{1}Users)(.*)(?=$)/]}/#{f}"><span class="material-symbols-outlined">
+                        rebase
+                        </span></div></td>
+                    STR
+                end 
+                @output << "</tr>"
+            }
+        @output << "</table></div>"
+    end
+    } 
+    end
     attr :output; true
-    attr :data
+    attr :data;
+    attr_writer :data
  end
+
+class History 
+    @@open_file = nil
+    @@date = nil
+    @@contentYML
+    def initialize(username, rootDirectory)
+        @username, @rootDirectory = username, rootDirectory
+    end
+    def init(type, data)
+        @@date = Time.now
+        data[data.keys[0]][:date] = @@date.strftime("%m/%d/%Y at %I:%M %p")
+        if type == 'init' 
+            puts "history_init.."
+            @@contentYML = "---\n:tasks:\n- :#{data.keys[0]}:\n"
+            data[data.keys[0]].each { |key, value|
+                if value != nil
+                    @@contentYML << "  :#{key}: #{value}\n"
+                end
+            }
+            File.open("#{@rootDirectory}/history.yml", 'w') do |content|
+                content.write(@@contentYML) 
+            end
+        elsif type == 'update'
+            puts "history_update.."
+            @@open_file = YAML::Store.new("#{@rootDirectory}/history.yml")
+            @@open_file.transaction do 
+            @@open_file[:tasks] << data.transform_keys(&:to_sym)
+            end
+        end
+    end
+end
 
  def implementJS(file)
     root_project = "/Users/ahmedabu-hassan/Desktop/uchi"
@@ -204,3 +297,4 @@ class FindAllFolders
     f.close
     return out
  end
+
